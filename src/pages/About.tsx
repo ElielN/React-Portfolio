ba4f1e3e609e41079092defa69661b5fb59c3aa9
ElielN <eliel.souza@ufv.br>
2022-03-08 20:16:00 -0300

    Criaçao das paginasabout e projects

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,28 @@
 import { Header } from "../components/Header";
 
+import eliel from '../assets/images/eliel.jpeg';
+
+import '../styles/about.scss';
+
 export function About(){
     return (
         <div className="page-about">
             <Header />
-            <h1>About</h1>
+            <div className="content-about">
+                <aside>
+                    <img src={eliel} alt='Minha imagem' />
+                </aside>
+                <main className="content-text">
+                    <h1>About</h1>
+                    <p>
+                        Estudante de Ciência da Computação pela Universidade Federal de Viçosa
+                        <br/>
+                        Atualmente sou estagiário na empresa Doois Web, onde estou trabalhando com front-end
+                        <br/>
+                        Sou apaixonado por tecnologia e estou sempre buscando aprender coisas novas
+                    </p>
+                </main>
+            </div>
         </div>
     );
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,35 @@
 import { Header } from "../components/Header";
 
+import githubImg from "../assets/images/githubImage.svg";
+
+import "../styles/projects.scss";
+
 export function Projects(){
     return (
         <div className="page-projects">
             <Header />
-            <h1>Projects</h1>
+            <div className="content-projects">
+                <aside>
+                    <img src={githubImg} alt='Minha imagem' />
+                </aside>
+                <main className="content-text">
+                    <h1>Projects</h1>
+                    <p>
+                        Você poderá conferir alguns de meus projetos clicando no botão logo abaixo
+                        <br/>
+                        Lá você encontrará:
+                        <br/>
+                        . Projetos em ReactJS, como este portfólio
+                        <br/>
+                        . Projetos mobile usando Flutter e Android Nativo
+                        <br/>
+                        . Projetos usando Python 
+                    </p>
+                    <button className="github-button">
+                        GitHub
+                    </button>
+                </main>
+            </div>
         </div>
     );
 }

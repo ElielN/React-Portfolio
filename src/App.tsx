dfebe7504b17417c1f839546b831855a12874685
ElielN <eliel.souza@ufv.br>
2022-03-09 22:10:44 -0300

    Ajustes para deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Projects } from './pages/Projects';
 import { Social } from './pages/Social';
-
 import './styles/global.scss'
 
 function App() {
